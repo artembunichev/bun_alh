2dd4ef99fc92ccce2e5e83c7adc805582f19748c
spleekz <spleekz@zohomail.com>
2023-08-04 15:37:18 +0300

    use $bun_style_theme_create for creating css theme

--- theme/theme.ts
+++ theme/theme.ts
@@ -1,17 +1,16 @@
 namespace $ {
 
-	var { vary } = $mol_style_func
-
-	export var $bun_alh_theme = {
-
-		main: vary( '--bun_alh_theme_main' ),
-		main_light: vary( '--bun_alh_theme_main_light' ),
-		main_bold: vary( '--bun_alh_theme_main_bold' ),
-		border: vary( '--bun_alh_theme_border' ),
-		border_bold: vary( '--bun_alh_theme_border_bold' ),
-		scroll_back: vary( '--bun_alh_theme_scroll_back' ),
-		scroll_thumb: vary( '--bun_alh_theme_scroll_thumb' ),
-
-	}
+	export var $bun_alh_theme = $bun_style_theme_create(
+		'bun_alh_theme',
+		[
+			'main',
+			'main_light',
+			'main_bold',
+			'border',
+			'border_bold',
+			'scroll_back',
+			'scroll_thumb',
+		]
+	)
 
 }
